25ac822f1d2e66d8af43811fa232921f30890f33
jhondesignee <jhonata.oliveira.275@gmail.com>
2023-04-10 05:28:44 -0300

    update

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite"
 
+const repositoryName = process.env.GITHUB_REPOSITORY?.slice("/")[1]
+
 export default defineConfig({
-  base: "/horrible-design/",
+  base: repositoryName ?? "/",
   server: {
     port: 4000
   }
